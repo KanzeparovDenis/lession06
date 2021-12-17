f39887fa088c3186c2a5ec802f47fd4360265ad5
Денис <denis.kanzeparov51@gmail.com>
2021-12-17 19:27:41 +0300

    Initial commit

---
+++ src/main/java/org/vmk/dep508/io/rest/SerializationHelper.java
@@ -0,0 +1,80 @@
+package org.vmk.dep508.io.rest;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.log4j.Logger;
+
+import java.io.*;
+
+
+public class SerializationHelper<T extends Serializable> {
+
+    Class<T> serialazationType;
+
+    public SerializationHelper(Class<T> serialazationType) {
+        this.serialazationType = serialazationType;
+    }
+
+    private Logger log = Logger.getLogger(getClass());
+
+    ObjectMapper mapper = new ObjectMapper();
+
+
+    /*
+      Необходимо десереализовать объект из файла по указанному пути
+     */
+    public T loadFromFile(String path) {
+        try (ObjectInputStream file = new ObjectInputStream(new FileInputStream(path)))
+        {
+            return (T) file.readObject();
+        } catch (IOException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /*
+      Необходимо сохранить сереализованный объект в файл по указанному пути
+     */
+    public boolean saveToFile(String path, T toSave) {
+       try (ObjectOutputStream fileout = new ObjectOutputStream(new FileOutputStream(path))){
+           fileout.writeObject(toSave);
+           fileout.flush();
+           return true;
+       }  catch (FileNotFoundException e){
+           e.printStackTrace();
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
+       return false;
+    }
+
+    public String convertToJsonString(T toConvert) {
+        try {
+            String json = mapper.writeValueAsString(toConvert);
+            return json;
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public void writeJsonToStream(OutputStream output, T toWrite) {
+        try {
+            mapper.writeValue(output, toWrite);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public T parseJson(String json) {
+        try {
+            return mapper.readValue(json, serialazationType);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
